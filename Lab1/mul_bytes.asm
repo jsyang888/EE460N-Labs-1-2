ee82e720a5ef50f26f1543d6c5099d52d9783a22
Justin <justinyang467@gmail.com>
2026-09-11 10:43:50 -0500

    logic changes in mul_bytes.asm should work correctly

--- Lab1/mul_bytes.asm
+++ Lab1/mul_bytes.asm
@@ -1,31 +1,36 @@
 .ORIG x3000 
     LEA R0, Access
+    LDW R0, R0, #0
     LDB R1, R0, #0 ; x3100
-    LDB R1, R0, #1 ; x3101
+    LDB R2, R0, #1 ; x3101
+    AND R3, R3, #0 ; product
     AND R4, R4, #0 ; R4 will track overflow status
+
+    ADD R1, R1, #0 ; reset CC bits
+    BRz LoopOver
+
 Loop
-    ADD R2, R2, R1 ; R2 has product
-    BRn Overflow ; if the result suddenly becomes negative, overflow
-Resume_Loop
-    ADD R0, R0, #-1
-    BRz Loop_Over
-    BR Loop
+    ADD R3, R3, R2 ; R3 has product
+    ADD R1, R1, #-1
+    BRp Loop
 
 
+LoopOver
+    LEA R5, Negative256
+    LDW R5, R5, #0
+    ADD R5, R3, R5
+    BRn Result ; this means product is under 256, not overflowed
+    ADD R4, R4, #1 ; set the negative flag
 
 
-Overflow
-    ADD R4, R4, #1
-    BR Resume_Loop ; go back to loop and continue where left off
-
-Loop_Over
+Result
     STB R4, R0, #3 ; 1 -> MEM[x3103] 
-    STB R2, R0, #2 ; Result -> MEM[x3102]
+    STB R3, R0, #2 ; Result -> MEM[x3102]
 
-HALT
 
 Access
     .FILL x3100
-
+Negative256
+    .FILL xFF00
 
 .END
