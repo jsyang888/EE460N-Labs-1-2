b61279815c5cdc1552ca5a556e1337487801b06a
Justin <justinyang467@gmail.com>
2026-09-11 20:55:48 -0500

    forgot a HALT, and properly formatted labels

--- Lab1/mul_bytes.asm
+++ Lab1/mul_bytes.asm
@@ -9,28 +9,23 @@
     ADD R1, R1, #0 ; reset CC bits
     BRz LoopOver
 
-Loop
-    ADD R3, R3, R2 ; R3 has product
-    ADD R1, R1, #-1
-    BRp Loop
+Loop ADD R3, R3, R2 ; R3 has product
+     ADD R1, R1, #-1
+     BRp Loop
 
 
-LoopOver
-    LEA R5, Negative256
-    LDW R5, R5, #0
-    ADD R5, R3, R5
-    BRn Result ; this means product is under 256, not overflowed
-    ADD R4, R4, #1 ; set the negative flag
+LoopOver LEA R5, Negative256
+         LDW R5, R5, #0
+         ADD R5, R3, R5
+         BRn Result ; this means product is under 256, not overflowed
+         ADD R4, R4, #1 ; set the overflow flag
 
 
-Result
-    STB R4, R0, #3 ; 1 -> MEM[x3103] 
-    STB R3, R0, #2 ; Result -> MEM[x3102]
+Result  STB R4, R0, #3 ; 1 -> MEM[x3103] 
+        STB R3, R0, #2 ; Result -> MEM[x3102]
 
 
-Access
-    .FILL x3100
-Negative256
-    .FILL xFF00
-
+Access .FILL x3100
+Negative256 .FILL xFF00
+HALT
 .END
